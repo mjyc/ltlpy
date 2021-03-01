db3c2f259468363ceee4465701b65954f2a3dd9d
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2021-02-28 22:02:34 -0800

    Add VARIABLE test

--- __init__.py
+++ __init__.py
@@ -1,1 +1,20 @@
-hello = 1
+from typing import Optional, Union
+from enum import Enum
+
+
+class LTLOperators(Enum):
+    VARIABLE = 'variable'
+    NOT = 'not'
+    AND = 'and'
+    OR = 'or'
+    NEXT = 'next'
+    ALWAYS = 'always'
+    EVENTUALLY = 'eventually'
+
+
+class LTLFormula:
+    def __init__(
+        self, op: LTLOperators, value: Optional[Union["LTLFormula", bool]]
+    ) -> None:
+        self.op = op
+        self.next = value
--- test.py
+++ test.py
@@ -1,20 +1,8 @@
-from hypothesis import given, note, strategies as st
-from ltlpy import hello
+from hypothesis import given, strategies as st
+from ltlpy import LTLOperators, LTLFormula
 
 
-@given(st.lists(st.integers()), st.randoms())
-def test_shuffle_is_noop(ls, r):
-    ls2 = list(ls)
-    r.shuffle(ls2)
-    note("Shuffle: %r" % (ls2))
-    assert ls == ls2
-
-
-# content of test_sample.py
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    print("---hello", hello)
-    assert func(3) == 5
+@given(st.lists(st.booleans()))
+def test_var(b: bool) -> None:
+    f = LTLFormula(op=LTLOperators.VARIABLE, value=b)
+    assert f.next == b
