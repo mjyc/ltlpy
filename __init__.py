d24311dcdc500af672e63b32c18a51a229018bf9
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2021-02-28 23:28:45 -0800

    Add Or

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from typing import cast, Union
 
-LTLFormula = Union["LTLVariable", "LTLNot"]
+LTLFormula = Union["LTLVariable", "LTLNot", "LTLOr"]
 
 
 class LTLVariable:
@@ -13,13 +13,29 @@ class LTLNot:
         self.value = value
 
 
+class LTLOr:
+    def __init__(self, left: LTLFormula, right: LTLFormula) -> None:
+        self.left = left
+        self.right = right
+
+
 def interpret(formula: LTLFormula) -> Union[LTLFormula, bool]:
     if type(formula) is LTLVariable:
-        return formula.value
+        return cast(LTLVariable, formula).value
     if type(formula) is LTLNot:
         f = interpret(cast(LTLNot, formula).value)
         if type(f) is bool:
             return not f
         return LTLNot(cast(LTLFormula, f))
+    if type(formula) is LTLOr:
+        f0 = interpret(cast(LTLOr, formula).left)
+        f1 = interpret(cast(LTLOr, formula).right)
+        if f0 is True:
+            return True
+        if f1 is True:
+            return True
+        if f0 is False and f1 is False:
+            return False
+        return LTLOr(cast(LTLFormula, f0), cast(LTLFormula, f1))
 
     raise Exception("Invalid formula")
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from hypothesis import given, strategies as st
-from ltlpy import LTLVariable, LTLNot, interpret
+from ltlpy import LTLVariable, LTLNot, LTLOr, interpret
 
 
 @given(st.booleans())
@@ -13,3 +13,10 @@ def test_not(b: bool) -> None:
     f = interpret(LTLNot(LTLVariable(b)))
     assert type(f) is bool
     assert f is not b
+
+
+@given(st.booleans(), st.booleans())
+def test_or(b0: bool, b1: bool) -> None:
+    f = interpret(LTLOr(LTLVariable(b0), LTLVariable(b1)))
+    assert type(f) is bool
+    assert f is (b0 or b1)
